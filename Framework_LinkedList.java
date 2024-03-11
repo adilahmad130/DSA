1d659679a0d0bf912593fd2910c7d3993b11141a
adilahmad130 <118704677+adilahmad130@users.noreply.github.com>
2024-03-11 11:42:16 +0530

    Add files via upload

---
+++ Framework_LinkedList.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+class LL{
+    public static void main(String args[]){
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+
+        list.addFirst("this");
+        list.add("list");
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i) + " -> ");
+        }
+        System.out.println("NULL");
+
+        list.removeFirst();
+        System.out.println(list);
+
+        list.removeLast();
+        System.out.println(list);
+
+        list.remove(1);
+        System.out.println(list);
+
+    }
+
+}
+
