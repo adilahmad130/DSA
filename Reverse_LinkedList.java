40cf25986ecf6a8a653df314c44212440ec6df77
adilahmad130 <118704677+adilahmad130@users.noreply.github.com>
2024-03-12 14:52:30 +0530

    Add files via upload

---
+++ Reverse_LinkedList.java
@@ -0,0 +1,139 @@
+class LL {
+    Node head;
+    private int size;
+
+    LL(){
+        this.size = 0;
+    }
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;  
+            size++; 
+        }
+        
+    }
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null)
+        {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+
+    }
+
+    public void printList(){
+        if(head == null){
+            System.out.println("List is Empty");
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode != null){
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+
+    }
+
+    public void deleteFirst(){
+        if(head == null)
+        {
+            System.out.println("List is Empty");
+        }
+        size--;
+        head = head.next;
+
+        
+    }
+
+    public void deleteLast(){
+        if(head == null)
+        {
+            System.out.println("List is Empty");
+            return;
+        }
+        size--;
+
+        if(head.next == null){
+            head = null;
+            return;
+        }
+
+        Node secondLast = head;
+        Node lastNode =  head.next;
+
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+
+        secondLast.next = null;
+
+    }
+
+    public int getSize(){
+        return size;        
+    }
+
+    public void reverseIterate(){
+        if(head == null || head.next == null){
+            return;
+        }
+        
+        Node prevNode = head;
+        Node currNode = head.next;
+
+        while(currNode!=null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+
+            //update
+            prevNode =  currNode;
+            currNode =  nextNode;
+
+        }
+        head.next = null;
+        head = prevNode;
+
+    }
+    public static void main(String args[])
+    {
+        LL list = new LL();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.printList();
+ 
+        list.reverseIterate();
+        System.out.println("");
+        System.out.println("Linked List After Reversal: ");
+        list.printList();
+        
+    }
+}
