95a6261857c97e7c6f7d1171bbea63f53262f42a
Adil Ahmad <118704677+adilahmad130@users.noreply.github.com>
2024-04-01 16:05:04 +0530

    Add files via upload

---
+++ QueueUsingArray.java
@@ -0,0 +1,68 @@
+public class QueueUsingArray{
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear = -1;
+
+        Queue(int n){
+            arr = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        public static void enqueue(int data){
+            if(rear == size - 1){
+                System.out.println("Full Queue");
+                return;
+            }
+
+            rear++;
+            arr[rear] = data;
+        }
+
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            
+            int front = arr[0];
+            
+            for(int i = 0;i < rear;i++){
+                arr[i] = arr[i+1];
+            }
+
+            rear--;
+            return front;
+
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+
+            return arr[0];
+        }
+        
+    }
+
+    public static void main(String args[]){
+        Queue q = new Queue(5);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        
+        
+
+        while(!q.isEmpty()){
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
