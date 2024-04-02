a4cc9dd494a09e2c26626b02482d9b3220cfcd51
Adil Ahmad <118704677+adilahmad130@users.noreply.github.com>
2024-04-02 12:11:25 +0530

    Add files via upload

---
+++ QueueUsingFrameworkLL.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueUsingFrameworkLL{
+    public static void main(String args[]) {
+        Queue<Integer> q = new LinkedList();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        System.out.println("Queue: ");
+
+        while (!q.isEmpty()) {
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
