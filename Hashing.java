a36653768ecd7a04b6f0561079eaf1a080300274
Adil Ahmad <118704677+adilahmad130@users.noreply.github.com>
2024-07-26 16:34:24 +0530

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Hashing {
+    public static void main(String args[]){
+        HashSet<Integer> set = new HashSet<>();
+
+        //Insert
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        //Size
+
+        System.out.println("Size of Set is: " + set.size());
+
+        System.out.println("Set: " + set);
+
+        //Search - contains
+
+        if(set.contains(1)){
+            System.out.println("Set Contains 1");
+        }
+        if(!set.contains(6)){
+            System.out.println("Does Not Contain");
+        }
+
+        //Delete
+
+        set.remove(1);
+        if(!set.contains(1)){
+            System.out.println("Does Not Contain 1 - We Deleted 1");
+        }
+
+        //Iterator
+
+        set.add(1);
+
+        Iterator it = set.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+        
+        //Checking if Set is Empty
+        if(!set.isEmpty()){
+            System.out.println("Set is Not Empty");
+        }
+    }
+}
